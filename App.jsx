1d623b278d722f6ae0c94ba247606fad3e0478c2
camisbersann <camila.bersan@aluno.senai.br>
2024-02-21 09:52:03 -0300

    feat: Gerência da lista e função de adicionar nova tarefa

--- App.jsx
+++ App.jsx
@@ -5,9 +5,17 @@ import { useState } from "react";
 
 export default function App() {
   const [task, setTask] = useState('');
-  
+  const [tasks, setTasks] = useState('');
+
+  const addTask = () => {
+    if (task.trim().length > 0) {
+      setTasks([...tasks, { id: Math.random().toString(), value: task }]);
+      setTask("");
+    }
+  };
+
   return (
-    <View style= {styles.container}>
+    <View style={styles.container}>
       <View style={styles.form}>
         <TextInput
           placeholder="Digite uma nova tarefa"
@@ -15,11 +23,19 @@ export default function App() {
           onChangeText={setTask}
           value={task}
         />
-        <TouchableOpacity
+        <TouchableOpacity onPress={addTask}
           style={styles.button}
         >
           <Text style={styles.buttonText}>Adicionar</Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.listTitle}>
+        {tasks.length === 0 ? (
+          <Text style={styles.emptyList}>Nenhuma Tarefa Cadastrada</Text>
+        ) : (
+          <Text style={styles.filledList}>Tarefas Cadastradas</Text>
+        )}
       </View>
       <StatusBar style="auto" />
     </View>
@@ -30,7 +46,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 50,
   },
-  
+
   form: {
     display: "flex",
     flexDirection: 'row',
@@ -48,11 +64,28 @@ const styles = StyleSheet.create({
 
   button: {
     backgroundColor: "blue",
-    padding: 5,
+    padding: 10,
     borderRadius: 5
   },
 
   buttonText: {
     color: "white",
+  },
+
+  listTitle: {
+    marginBottom: 10,
+    alignItems: "center"
+  },
+
+  emptyList: {
+    textAlign: "center",
+    color: "red",
+    fontSize: 20
+  },
+
+  filledList: {
+    textAlign: "center",
+    color: "green",
+    fontSize: 20
   },
 });
