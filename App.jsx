4c6086186c69082b0dfd611b4075cf05c39a4d6c
camisbersann <camila.bersan@aluno.senai.br>
2024-02-21 09:20:12 -0300

    refactor: configuração inicial

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+	return (
+		<View style ={styles.container}>
+      <Text>Hello, World!</Text>
+      <StatusBar styles="auto" />
     </View>
-  );
+	);
 }
 
 const styles = StyleSheet.create({
